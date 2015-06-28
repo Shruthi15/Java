c7909bd2c872927972423d3230cd6b7b746bb35a
Shruthi <Shruthi15@users.noreply.github.com>
2015-06-27 23:42:42 -0400

    Create LongestCommonSubsequence.java

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,66 @@
+/* Length of Longest Common Subsequence - Dynamic Programming */
+
+package stringunique;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LongestCommonSubsequence {
+
+	public int  lcs(String str1, String str2)
+    {
+        int l1 = str1.length();
+        int l2 = str2.length();
+ 
+        int[][] arr = new int[l1 + 1][l2 + 1];
+        int max = 0;
+ 
+        for (int i = 1; i < l1 + 1; i++)
+        {
+            for (int j = 1; j < l2 + 1; j++)
+            {
+                arr[i][j] = Math.max(arr[i][j-1],arr[i-1][j]);
+            	
+            	if (str1.charAt(i - 1) == str2.charAt(j - 1))
+                {
+                        arr[i][j] = arr[i - 1][j - 1] + 1;
+                        if (arr[i][j] > max)
+                        {
+                            max = arr[i][j];
+                            
+                        }               
+                }
+                
+            }
+        }        
+ 
+        return max;
+    }
+ 
+    public static void main(String[] args) throws IOException
+    {    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+ 
+        System.out.println("\nEnter string 1");
+        String str1 = br.readLine();
+ 
+        System.out.println("\nEnter string 2");
+        String str2 = br.readLine();
+ 
+        LongestCommonSubsequence lcs = new LongestCommonSubsequence(); 
+        int result = lcs.lcs(str1, str2);
+ 
+        System.out.println("\n Length of Longest Common Subsequence : "+ result);
+    }
+}
+	
+	
+	
+	
+	
+	
+	
+	
+
